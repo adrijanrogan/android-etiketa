1d809114a33003f724b95b9806b883bdb4dac7a9
Adrijan Rogan <adrijanrogan@protonmail.com>
2019-06-25 11:31:48 +0200

    Add support for sorting by last modified.
    Similar to name sorting, grouping is supported. If two files have the same last modified, the name is compared. Also extracted some common logic for comparing directories. Also fixed SORT_MODE_FILTER in case more sorting modes are added. In addition, menu string were changed from "name ascending/descending" to simply "ascending/descending".

--- app/src/main/java/com/github/adrijanrogan/etiketa/util/FileComparator.kt
+++ app/src/main/java/com/github/adrijanrogan/etiketa/util/FileComparator.kt
@@ -1,34 +1,63 @@
 package com.github.adrijanrogan.etiketa.util
 
+import android.util.Log
 import java.io.File
 import java.util.*
 
 class FileComparator(private val sortMode: Int) : Comparator<File> {
 
     override fun compare(o1: File, o2: File): Int {
-        val type = sortMode or SORT_MODE_FILTER
+        Log.d("FileComparator", "Sort mode is $sortMode")
+        val type = sortMode and SORT_MODE_FILTER
         val asc = sortMode and SORT_MODE_REVERSED == 0
         val ascending = if (asc) 1 else -1
         val group = sortMode and SORT_MODE_DO_NOT_GROUP == 0
 
         val result = when (type) {
             SORT_MODE_FILENAME -> compareByName(o1, o2, group)
+            SORT_MODE_LAST_MODIFIED -> compareByLastModified(o1, o2, group)
             else -> compareByName(o1, o2, group)
         }
 
         return ascending * result
     }
 
+    private fun compareDirectories(first: File, second: File, block: () -> Int): Int {
+        return when {
+            first.isDirectory == second.isDirectory -> block.invoke()
+            first.isDirectory -> -1
+            else -> 1
+        }
+    }
+
     private fun compareByName(first: File, second: File, group: Boolean): Int {
+        Log.d("FileComparator", "compareByName($first, $second, $group)")
         return if (group) {
-            when {
-                first.isDirectory == second.isDirectory -> compareByName(first, second, false)
-                first.isDirectory -> -1
-                else -> 1
+            return compareDirectories(first, second) {
+                compareByName(first, second, false)
             }
         } else {
             first.name.toLowerCase().compareTo(second.name.toLowerCase())
         }
+    }
+
+    private fun compareByLastModified(first: File, second: File, group: Boolean): Int {
+        Log.d("FileComparator", "compareByLastModified()")
+        return if (group) {
+            return compareDirectories(first, second) {
+                compareByLastModified(first, second, false)
+            }
+        } else {
+            val last1 = first.lastModified()
+            val last2 = second.lastModified()
+            if (last1 == last2)
+                return compareByName(first, second, false)
+             booleanToInt(last1 >= last2)
+        }
+    }
+
+    private fun booleanToInt(bool: Boolean): Int {
+        return if (bool) -1 else 1
     }
 
 
@@ -40,7 +69,7 @@ class FileComparator(private val sortMode: Int) : Comparator<File> {
         const val SORT_MODE_FILENAME = 0x000
         const val SORT_MODE_LAST_MODIFIED = 0x100
 
-        const val SORT_MODE_FILTER = 0x100
+        const val SORT_MODE_FILTER = 0xF00
 
         // "Second" bit:
         // 0: ascending (normal)
